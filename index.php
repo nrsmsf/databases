399337cab0ba726547755626907cb58322ea5e1a
root <root@localhost>
2018-11-26 02:38:00 +0300

    добавлен первый тестовый запрос

--- index.php
+++ index.php
@@ -11,16 +11,19 @@
 <body>
 
 <h1>Показ работы</h1>
-<?
+<?php
+ini_set('display_errors',1);
 error_reporting(E_ALL);
 mb_internal_encoding("UTF-8");
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $link  =  mysqli_connect( '80.93.177.12',  "admin_nrsmsf",  'database',  'admin_database' );
 if (  !$link  )  die("Error");
-
-mysqli_select_db("admin_database");
-
-
-printf(mysqli_fetch_row(mysqli_query( $link,  "SELECT * FROM Товар WHERE `Код_товара` = 1")));
+mysqli_set_charset($link, "utf8");
+$result2 = mysqli_query( $link,  "select * FROM Фирма WHERE `id` = 0");
+$row2 = mysqli_fetch_row($result2);
+$balance2 = $row2[1];
+echo "Товар 1: {$balance2} ";
 ?>
 
 </body>
